b81830cc83be9e4b9d6206fc41c7ac1f049e0794
avi.silverman <avi.silverman@3d90d51c-b2b8-b82a-2a88-0198ea863bd9>
2013-02-25 21:56:27 +0000

    Call slip updates.

--- javascripts.js
+++ javascripts.js
@@ -19,3 +19,47 @@ function flips(name){
 	//else
 	//	alert("N - "+name.checked);
 }
+
+
+//function onGridFieldChanged(el){
+//var form = XataJax.load('XataJax.form');
+//var field = jQuery('#inventory_id');
+//var sellcost = form.findField(field, 'sell_cost');
+//alert(field);
+//}
+
+//require <xatajax.form.core.js>
+/*(function(){
+
+    // Make onGridFieldChanged function in the global scope
+     //  FOR DEMONSTRATION ONLY... YOU SHOULD USE YOUR OWN CONSISTENT
+     // NAMESPACING AND NOT CLUTTER THE GLOBAL SCOPE
+    window.onGridFieldChanged = onGridFieldChanged; 
+*/
+/*
+    * A callback to handle when an ingredient is changed.  This is registered
+    * in the dtg_recipe_ingredients fields.ini file.
+    * @param {HTMLElement} el The field that was changed.
+    */
+	function onGridFieldChanged(el){
+		var $ = jQuery;
+		var form = XataJax.load('XataJax.form');
+                // get the other fields in the current row
+                //var taxPercentField = xform.findField(el, 'taxPercent');
+                //var taxAmountField = xform.findField(el, 'taxPercent');
+                //var priceField = xform.findField(el, 'price');
+                //var totalField = xform.findField(el, 'price');
+
+				var iid = $('#inventory_id');
+				//var sellcost = form.findField(iid, 'sell_cost');
+
+                //$( sellcost).val(1) );
+				//alert(el);
+				//alert($(iid).val());
+				
+				alert($(el).attr('id'))
+      
+      
+      
+   }
+//})();
--- tables/time_logs/time_logs.php
+++ tables/time_logs/time_logs.php
@@ -1,33 +1,25 @@
 <?php
 
 class tables_time_logs {
+	private $rate_type;
 
-
-/*	function block__after_rate_label_widget(){
-		echo '<table class="rate_codes"><tr><td>';
+	function rate_id__default(){
+		return 1;
 	}
 
-	function block__after_supr_tt_widget(){
-		echo '</td></tr></table>';
+	function rate_type__validate(&$record, $value, &$params){
+		$this->rate_type = $value;
+		return 1;
 	}
-*/
-
-/*	function rate_data__renderCell(&$record){
-		$result = '<table class="rate_codes">';
-		foreach ( $record->val('rate_data') as $vals){ $result .= '<tr><th>' . $vals['type'] . '</th><th>' . $vals['rate'] . '</th></tr>'; };
-		$result .= "</table>";
-
-		return $result;
+	
+	function beforeSave(&$record){
+		if($this->rate_type == 'no_chrg')
+			$record->setValue('rate_per_hour', 0.00);
+		else{
+			$rate_rec = df_get_record('rates', array('rate_id'=>$record->val('inventory_id')));
+			$record->setValue('rate_per_hour', $rate_rec->val($this->rate_type));
+		}
 	}
-
-	function rate_data__htmlValue(&$record){
-		$result = '<table class="rate_codes">';
-		foreach ( $record->val('rate_data') as $vals){ $result .= '<tr><th>' . $vals['type'] . '</th><th>' . $vals['rate'] . '</th></tr>'; };
-		$result .= "</table>";
-
-		return $result;
-	}
-*/	
 	
 }
 
