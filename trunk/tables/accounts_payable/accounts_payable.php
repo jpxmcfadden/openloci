93452b45ee2fe79f2f98617290539edf7027511d
avi.silverman@gmail.com <avi.silverman@gmail.com@3d90d51c-b2b8-b82a-2a88-0198ea863bd9>
2013-11-07 21:36:25 +0000

    Added status pending button to Accounts Payable records

--- trunk/tables/accounts_payable/accounts_payable.php
+++ trunk/tables/accounts_payable/accounts_payable.php
@@ -45,14 +45,72 @@ class tables_accounts_payable {
 		);
 	}
 */
+	function section__status(&$record){
+		$app =& Dataface_Application::getInstance(); 
+		$query =& $app->getQuery();
+		$childString = '';
+
+		//If the "Change Status To: Pending" button has been pressed.
+		//Because both the $_GET and $query will be "" on a new record, check to insure that they are not empty.
+		if(($_GET['-pending'] == $query['-recordid']) && ($query['-recordid'] != "")){
+			$record->setValue('post_status',"Pending"); //Set status to Pending.
+			$res = $record->save(null, true); //Save Record w/ permission check.
+
+			//Check for errors.
+			if ( PEAR::isError($res) ){
+				// An error occurred
+				//throw new Exception($res->getMessage());
+				$msg = '<input type="hidden" name="--error" value="Unable to change status. This is most likely because you do not have the required permissions.">';
+			}
+			else
+				$msg = '<input type="hidden" name="--msg" value="Status Changed to: Pending">';
+			
+			$childString .= '<form name="status_change">';
+			$childString .= '<input type="hidden" name="-table" value="'.$query['-table'].'">';
+			$childString .= '<input type="hidden" name="-action" value="'.$query['-action'].'">';
+			$childString .= '<input type="hidden" name="-recordid" value="'.$record->getID().'">';
+
+			$childString .= $msg;
+
+			$childString .= '</form>';
+			$childString .= '<script language="Javascript">document.status_change.submit();</script>';
+		}
+		elseif(	$record->val('post_status') == ''){
+			$childString .= '<form>';
+			$childString .= '<input type="hidden" name="-table" value="'.$query['-table'].'">';
+			$childString .= '<input type="hidden" name="-action" value="'.$query['-action'].'">';
+			$childString .= '<input type="hidden" name="-recordid" value="'.$record->getID().'">';
+			
+			$childString .= '<input type="hidden" name="-pending" value="'.$record->getID().'">';
+			$childString .= '<input type="submit" value="Change Status to: Pending">';
+
+			$childString .= '</form>';
+		}
+		//elseif(	$record->val('post_status') == 'Pending'){ //---can do this by linking to -action=ledger_post&selected="this_one"
+		//	$childString .= 'Post';
+		//}
+		else {
+			$childString .= "No further options available";
+		}
+		
+		//if(	$record->val('post_status') == '')
+		return array(
+			'content' => "$childString",
+			'class' => 'main',
+			'label' => 'Change Status',
+			'order' => 10
+		);
+	}
+
+
+
+
 
 	function afterSave(&$record){
-		
-		
-		$po_rec = df_get_record('accounts_payable_unassigned_purchase_orders', array('purchase_order_id_full'=>$record->val('purchase_order_id')));
-		$po_rec->setValue('assigned_voucher_id',$record->val('voucher_id'));
-		//$gen_inv->save(null, true);
-		$po_rec->save();
+			
+	//	$po_rec = df_get_record('accounts_payable_unassigned_purchase_orders', array('purchase_order_id_full'=>$record->val('purchase_order_id')));
+	//	$po_rec->setValue('assigned_voucher_id',$record->val('voucher_id'));
+	//	$po_rec->save();
 		//return PEAR::raiseError('END',DATAFACE_E_NOTICE);
 	}
 }
