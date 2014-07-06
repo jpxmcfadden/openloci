efbb444240e88bda69618a53339f3cb219d9f2d5
avi.silverman@gmail.com <avi.silverman@gmail.com@3d90d51c-b2b8-b82a-2a88-0198ea863bd9>
2014-07-06 01:39:35 +0000

    Added more to close ledger action.

--- actions/close_ledger.php
+++ actions/close_ledger.php
@@ -10,73 +10,134 @@ class actions_close_ledger {
 		if ( !isUser() )
 			return Dataface_Error::permissionDenied("You are not logged in");
 			//header('Location: index.php?-action=login_prompt'); //Go to dashboard.
-			
-		//Period type check
-		if( isset($_GET['-period_type']) ){
-			$period_type = $_GET['-period_type'];
-			if($period_type != "month" && $period_type != "quarter" && $period_type != "year"){
-				df_display(array("period_type" => "Unrecognized Period Type"), 'close_ledger.html');
-				return 0;
-			}
-		}
-		else{
-			df_display(array("period_type" => "Period Type not Defined"), 'close_ledger.html');
-			return 0;
-		}
-
-		//If Period range has not been selected -> 1) Get months with non-closed posted entries
-		if(!isset($_GET['period_range'])){
-			$records = df_get_records_array("general_ledger", array("ledger_status"=>"Posted"));
-			$dates = array();
-			foreach($records as $record){
-				$record = $record->vals();
-				
-				if($period_type == "month")
-					$date_str = date("Y F",strtotime($record['ledger_date']['year'] . '-' . $record['ledger_date']['month']));
-				elseif($period_type == "year")
-					$date_str = $record['ledger_date']['year'];
-				elseif($period_type == "quarter"){
-					$quarter = ceil(date('m',strtotime($record['ledger_date']['year'] . "-" . $record['ledger_date']['month']))/3);
-					$date_str = $record['ledger_date']['year'] . ' - Quarter #' . $quarter;
+		
+		$action_section = isset($_GET['action_section']) ? $_GET['action_section'] : NULL;
+		$data_array = array();
+		
+		switch($action_section){
+			case NULL: { //Initial State: Select Period based on Period Type
+				//Check period_type to make sure it exists and is sane.
+				if( isset($_GET['-period_type']) ){
+					$period_type = $_GET['-period_type'];
+					if($period_type != "month" && $period_type != "quarter" && $period_type != "year"){
+						$data_array = array_merge($data_array, array("period_type" => "Unrecognized Period Type"));
+						break;
+					}
+				}
+				else{
+					$data_array = array_merge($data_array, array("period_type" => "Period Type not Defined"));
+					break;
 				}
 
-				$dates[$date_str] = $date_str;
-			}
-			
-			df_display(array("period_type"=>ucfirst($period_type), "period_select"=>$dates), 'close_ledger.html');		
-		}
+				//Get months with non-closed posted entries
+				$records = df_get_records_array("general_ledger", array("ledger_status"=>"Posted"));
+				$dates = array();
 
-		
-		//Pull all records in the period
-		$date_range = ">=".date(2014);
-		$records = df_get_records_array("general_ledger", array("ledger_date"=>$date_range));
-			
+				//Parse through all records in the General Ledger with entries that have a Posted status & create a list of options based on this and the selected Period Type (M/Y/Q)
+				//$date_str is what will show in the dropdown, $date_rng will be the value passed back for selecting the appropriate records
+				foreach($records as $record){
+					$record = $record->vals();
+						
+					if($period_type == "month"){
+						$date_str = date("Y F",strtotime($record['ledger_date']['year'] . '-' . $record['ledger_date']['month']));
+						$date_rng = ">=" . date("Y-m-1",strtotime($record['ledger_date']['year'] . '-' . $record['ledger_date']['month'])) . " AND <=" . date("Y-m-t",strtotime($record['ledger_date']['year'] . '-' . $record['ledger_date']['month']));
+					}
+					elseif($period_type == "year"){
+						$date_str = $record['ledger_date']['year'];
+						$date_rng = ">=" . $record['ledger_date']['year'] . "-01-01 AND <=" . $record['ledger_date']['year'] . "-12-31";
+					}
+					elseif($period_type == "quarter"){
+						//$quarter = ceil(date('m',strtotime($record['ledger_date']['year'] . "-" . $record['ledger_date']['month']))/3);
+						$quarter = ceil($record['ledger_date']['month'] / 3);
+						$date_str = $record['ledger_date']['year'] . ' - Quarter #' . $quarter;
+						$date_rng = ">=" . $record['ledger_date']['year'] . '-' . ((($quarter - 1)*3)+1) . "-01 AND <=" .  date("Y-m-t",strtotime($record['ledger_date']['year'] . '-' . ($quarter *3) . '-01'));
+					}
 
-		//Check for unposted batches in the period
-		$unposted_records = array(); //Create empty array
-		foreach($records as $record){ //Cycle through records to check for unposted ones, add to array
-			if($record->val('post_status') != "Posted"){
-				$unposted_records = array_merge($unposted_records,array($record->vals()));
-			}
-		}
-		
-		//If unposted batches, show them with error message
-		if(count($unposted_records) > 0){
-			df_display(array("period_type"=>ucfirst($period_type), "unposted"=>$unposted_records), 'close_ledger.html');
-			return 0;
-		}
-
-
-		
-			//If no unposted batches - 
-				//Show / print trial balance, Income Statement & Balance Sheets
-				//press submit button to confirm
+					//URL Encode $date_rng, so that it can be passed as a $_GET parameter
+					$dates[$date_str] = urlencode($date_rng);
+				}
+					
+				$data_array = array_merge($data_array, array("period_type"=>ucfirst($period_type), "period_select"=>$dates));
 				
-					//Change Status to "Closed"
-					//Create entry in table status
+			} break;
 			
-		df_display(array("period_type" => ucfirst($period)), 'close_ledger.html');		
-		
+			case "display": { //Check for unposted batches & show, Show Trial Balance / Income Statement / Balance Sheets
+				//Get & decode the period range
+				$date_range = urldecode($_GET['period_range']);
+				$data_array = array_merge($data_array, array("period_range"=>$_GET['period_range'])); //Add period_range - going to need to use it again after confim button is pressed.
+
+				//Pull all records in the period
+				$records = df_get_records_array("general_ledger", array("ledger_date"=>$date_range));
+
+				//Check for unposted batches in the period
+				$unposted_records = array(); //Create empty array
+				foreach($records as $record){ //Cycle through records to check for unposted ones, add to array
+					if($record->val('post_status') != "Posted"){
+						$unposted_records = array_merge($unposted_records,array($record->vals()));
+					}
+				}
+				
+				//If unposted batches, show them with error message
+				if(count($unposted_records) > 0){
+					$data_array = array_merge($data_array, array("unposted"=>$unposted_records));
+					break;
+				}
+
+				//If no unposted batches - Show / print Trial Balance, Income Statement & Balance Sheets
+				$account_data = array();
+				$total_debit = 0;
+				$total_credit = 0;
+				foreach($records as $record){
+					$journal_entries = df_get_records_array("general_ledger_journal", array("ledger_id" => $record->val("ledger_id")));
+					//foreach($journal_entries as $journal_entry){ //Initialize offsets/indexes - i.e. get rid of "notice"s - can get rid of this
+					//	$account_data[$journal_entry->val('account_id')] = "";
+					//	$account_data[$journal_entry->val('account_id')]["credit"] = 0;
+					//	$account_data[$journal_entry->val('account_id')]["debit"] = 0;
+					//}
+					foreach($journal_entries as $journal_entry){
+					echo "(".$account_data[$journal_entry->val('account_id')]['debit'].")(".$account_data[$journal_entry->val('account_id')]['credit'].")<BR>"; 
+						$account_data[$journal_entry->val('account_id')]['debit'] += $journal_entry->val('debit');
+						$account_data[$journal_entry->val('account_id')]['credit'] += $journal_entry->val('credit');
+						$total_debit += $journal_entry->val('debit');
+						$total_credit += $journal_entry->val('credit');
+						//echo $journal_entry->val('ledger_id') . ":" . $journal_entry->val('journal_id'). " :: ". $journal_entry->val('account_id') . " -- Debit = " . $journal_entry->val('debit') . ", Account Debit = " . $account_data[$journal_entry->val('account_id')]['debit'] . ", Total Debit = " . $total_debit . "<br>";
+						//echo $journal_entry->val('ledger_id') . ":" . $journal_entry->val('journal_id') . " :: ". $journal_entry->val('account_id') . " -- Credit = " . $journal_entry->val('credit') . ", Account Credit = " . $account_data[$journal_entry->val('account_id')]['credit'] . ", Total Credit = " . $total_credit . "<br><br>";
+					}
+				}
+				ksort($account_data); //use ksort() to sort account numbers numerically accending
+				
+				//Add account information to $account_data
+				foreach($account_data as $account_id => $account){
+					$account_record = df_get_record("chart_of_accounts", array("account_id" => $account_id));
+					$account_data[$account_id]["account_number"] = $account_record->val("account_number");
+					$account_data[$account_id]["account_name"] = $account_record->val("account_name");
+					$account_data[$account_id]["debit"] = number_format($account_data[$account_id]["debit"],2); //Convert to 2 decimals
+					$account_data[$account_id]["credit"] = number_format($account_data[$account_id]["credit"],2); //Convert to 2 decimals
+				}
+				$total_debit = number_format($total_debit, 2);  //Convert to 2 decimals
+				$total_credit = number_format($total_credit, 2);  //Convert to 2 decimals
+				
+				$data_array = array_merge($data_array, array("accounts"=>$account_data,"total_debit"=>$total_debit,"total_credit"=>$total_credit)); //Add account data & credit/debit totals
+			
+			} break;
+
+			case "confirm": {
+				//Change Status to "Closed"
+				//Create entry in table status		
+			
+			} break;
+			
+			default: {
+				echo "SOMETHING BROKE!"; //use msg paramater
+			}
+		}
+
+//echo "<pre>";
+//print_r($data_array);		
+//echo "</pre>";
+
+		df_display($data_array, 'close_ledger.html');		
+		return 0;				
 	}
 }
 
